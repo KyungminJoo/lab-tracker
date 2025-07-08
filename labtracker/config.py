a400db980f636a69f31a7ae78e5a6caacd9dc80e
KyungminJoo <greenscout@naver.com>
2025-07-08 17:43:01 +0900

    feat: allow label size via env

--- labtracker/config.py
+++ labtracker/config.py
@@ -14,3 +14,12 @@ class Config:
     PRINTER_NAME = os.getenv("PRINTER_NAME", "Label")
     SITE_URL = os.getenv("SITE_URL", "http://localhost:15000")
     LABEL_OUTPUT_DIR = os.getenv("LABEL_OUTPUT_DIR")
+    try:
+        LABEL_WIDTH_PX = int(os.getenv("LABEL_WIDTH_PX", "400"))
+    except ValueError:
+        LABEL_WIDTH_PX = 400
+
+    try:
+        LABEL_HEIGHT_PX = int(os.getenv("LABEL_HEIGHT_PX", "180"))
+    except ValueError:
+        LABEL_HEIGHT_PX = 180
